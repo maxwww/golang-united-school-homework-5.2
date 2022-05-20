0390b134dabd405eae4220df39f8884ea3bd63d3
ms76adf1 <ms76adf1@gmail.com>
2022-05-20 12:45:16 +0300

    implement Cache module

--- cache.go
+++ cache.go
@@ -2,22 +2,56 @@ package cache
 
 import "time"
 
+type EmptyStruct struct{}
 type Cache struct {
+	keyValues   map[string]string
+	deadlineMap map[string]chan EmptyStruct
 }
 
 func NewCache() Cache {
-	return Cache{}
+	keyValues := make(map[string]string)
+	deadlineMap := make(map[string]chan EmptyStruct)
+	return Cache{
+		keyValues,
+		deadlineMap,
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	value, ok := c.keyValues[key]
+	return value, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.keyValues[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	result := []string{}
+	for k := range c.keyValues {
+		result = append(result, k)
+	}
+
+	return result
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	now := time.Now()
+	if now.Before(deadline) {
+		if deadlineChan, ok := c.deadlineMap[key]; ok {
+			deadlineChan <- EmptyStruct{}
+		}
+		c.Put(key, value)
+		go func() {
+			deadlineChan := make(chan EmptyStruct)
+			c.deadlineMap[key] = deadlineChan
+			timer := time.NewTimer(deadline.Sub(now))
+			select {
+			case <-deadlineChan:
+				return
+			case <-timer.C:
+				delete(c.keyValues, key)
+			}
+		}()
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maxwww/golang-united-school-homework-5.2
+
+go 1.18
